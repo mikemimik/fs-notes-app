7aee04e7ef6e595fc240cdf63411dcc0a93fe0dd
Michael Perrotte <michael.perrotte@auth0.com>
2021-04-16 13:14:49 -0400

    feat(api): add user controller/service
    - Added 'createUser' service function

--- api/routes/users/userController.js
+++ api/routes/users/userController.js
@@ -1,3 +1,24 @@
 const User = require('./userModel');
 
+exports.createUser = async ({
+  email,
+  password,
+  firstName,
+  lastName,
+}) => {
+  try {
+    const userDocument = new User({
+      email,
+      password,
+      firstName,
+      lastName,
+    });
+    await userDocument.save();
+
+    return userDocument;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
 
