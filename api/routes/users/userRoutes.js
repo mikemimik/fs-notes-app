8bdcf9062b8b43c02428c73809709ac56d40cd72
Michael Perrotte <michael.perrotte@auth0.com>
2021-04-21 00:25:26 -0400

    feat(api): update userRoutes
    - Add '/login' route handler

--- api/routes/users/userRoutes.js
+++ api/routes/users/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
-const { createUser } = require('./userController');
+const { createUser, findUserById } = require('./userController');
+const { verifyToken } = require('../../middleware/verifyToken');
 const { createToken } = require('../../tokens/tokenService');
 
 const router = express.Router();
@@ -8,6 +9,7 @@ const router = express.Router();
 router.route('/signup')
   .post(async (req, res) => {
     const { email, password, firstName, lastName } = req.body;
+
     if (!email || email === '') {
       return res.status(400).json({ message: 'email must be provided' });
     }
@@ -26,6 +28,7 @@ router.route('/signup')
 
     try {
       const user = await createUser({ email, password, firstName, lastName });
+      // NOTE: the 'payload' of the token is the parameter
       const token = createToken({ id: user._id });
 
       res.status(201).json({ access_token: token });
@@ -36,14 +39,45 @@ router.route('/signup')
     }
   });
 
+router.route('/login')
+  .post(async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || email === '') {
+      return res.status(400).json({ message: 'email must be provided' });
+    }
+
+    if (!password || password === '') {
+      return res.status(400).json({ message: 'password must be provided' });
+    }
+
+    try {
+      const user = await loginUser({ email, password });
+      const token = createToken({ id: user._id });
+
+      res.json({ access_token: token });
+    } catch (err) {
+      console.error(err);
+      if (err.message && err.message === 'unauthorized') {
+        return res.status(403).json({ message: 'unauthorized' });
+      }
+      return res.status(500).json({ message: 'internal server error' });
+    }
+  });
+
 router.route('/userinfo')
-  .get((req, res) => {
-    // TODO: protect this endpoint with middleware
-    // TODO: pull user data from request
-    // TODO: check if that user exists in the database
-    // TODO: return information about user
-    res.json({});
-  })
+  .get(verifyToken, async (req, res) => {
+    try {
+      const { id } = req.user;
+      const user = await findUserById(id);
+      res.json(user);
+    } catch (err) {
+      if (err.message && err.message === 'not found') {
+        return res.status(404).json({ message: 'user not found' });
+      }
+      return res.status(500).json({ message: 'internal server error' });
+    }
+  });
 
 
 module.exports = router;
