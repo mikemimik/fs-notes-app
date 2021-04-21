e6e5f40989b9f58c42573645280a0a8c1d9af50c
Michael Perrotte <michael.perrotte@auth0.com>
2021-04-20 23:59:18 -0400

    feat(api): update tokenService
    - Add 'verifyToken' function

--- api/tokens/tokenService.js
+++ api/tokens/tokenService.js
@@ -6,3 +6,14 @@ exports.createToken = (payload) => {
   const token = jwt.sign(payload, TOKEN_SECRET_KEY);
   return token;
 };
+
+exports.verifyToken = (token) => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, TOKEN_SECRET_KEY, (err, decoded) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(decoded);
+    });
+  });
+};
