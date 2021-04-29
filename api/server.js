dd4ea8f3cf1f589285f6cdf7fa60946e082f676d
Michael Perrotte <michael.perrotte@auth0.com>
2021-04-29 19:54:22 -0400

    feat(api): moved server file

--- api/server.js
+++ api/server.js
@@ -1,16 +1,25 @@
+const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 
-const userRouter = require('./api/routes/users/userRoutes');
-const notesRouter = require('./api/routes/notes/notesRoutes');
+const UserModel = require('./routes/users/userModel');
+const userRouter = require('./routes/users/userRoutes');
+const notesRouter = require('./routes/notes/notesRoutes');
 
 const app = express();
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use('/', express.static(path.join(__dirname, '../build')));
+
 app.use('/api/users', userRouter);
 app.use('/api/notes', notesRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 
 mongoose
   .connect('mongodb://localhost:27017/note-app',
@@ -19,9 +28,12 @@ mongoose
       useUnifiedTopology: true,
     },
   )
-  .then(() => {
+  .then(async () => {
+
+    await UserModel.deleteMany();
+
     app.listen('8080', () => {
       console.log('server is running on port 8080');
     });
   })
-  .catch((err) => console.log(err));
+  .catch((err) => console.log('wat:', err));
